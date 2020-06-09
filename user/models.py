53ad577e6df2a2b1c84bf66e8122705536349851
SkyF <SkyMXF@users.noreply.github.com>
2020-06-09 23:48:15 +0800

    c

--- user/models.py
+++ user/models.py
@@ -5,8 +5,14 @@ class UserInfo(models.Model):
     user_qq_id = models.BigIntegerField(unique=True)
     password = models.CharField(max_length=256)
 
+    def __str__(self):
+        return "qq: %s, pwd: %s"%(str(self.user_qq_id), str(self.password))
+
 class UserGroup(models.Model):
     # 一个qq可以属于多个群
     user_qq_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
     group_id = models.BigIntegerField()
     nickname = models.CharField(max_length=256)
+    
+    def __str__(self):
+        return "qq: %s, group_id: %s, nickname: %s"%(str(self.user_qq_id), str(self.group_id), str(self.nickname))
--- user/views.py
+++ user/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 def userinfo(request):
     return render(request, 'user/userinfo.html')
 
 def login(request):
+
+    if request.method == "POST":
+        userqq = request.POST.get('userqq')
+        password = request.POST.get('password')
+        if userqq.strip() and password:     # qq和密码不为空
+            # 验证输入qq为数字
+            try:
+                userqq = int(userqq)
+            except:
+                message = "输入QQ号格式错误"
+                return render(request, "user/login.html", {"message": message})
+
+            # 查询数据库
+            try:
+                user = models.UserInfo.objects.get(user_qq_id=userqq)
+            except:
+                message = "没有此QQ记录，请联系公会管理人员添加"
+                return render(request, 'user/login.html', {"message": message})
+            
+            if user.password == password:
+                return redirect("/")
+            else:
+                message = "密码不正确，如果忘记可私聊发送'重置密码'进行重置"
+                return render(request, 'user/login.html', {"message": message})
+
+        else:
+            message = "QQ或密码不能为空"
+            return render(request, "user/login.html", {"message": message})
+
     return render(request, 'user/login.html')
 
 def setpwd(request):
