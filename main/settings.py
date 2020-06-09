6a7ecb61e795e098bd7c5b15708769730ef7b651
SkyF <SkyMXF@users.noreply.github.com>
2020-06-09 19:27:56 +0800

    c

--- main/settings.py
+++ main/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
