1f315db11a47e21271d0c2b002821ac0cd748342
SkyF <SkyMXF@users.noreply.github.com>
2020-06-09 22:25:43 +0800

    c

--- user/views.py
+++ user/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def userinfo(request):
-    return render(request, 'userinfo.html')
+    return render(request, 'user/userinfo.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'user/login.html')
 
 def setpwd(request):
-    return render(request, 'setpwd.html')
+    return render(request, 'user/setpwd.html')
 
 def logout(request):
     return redirect("/login/")
