a45a68b6e21149d3aebdfb77c9bfc443df18c946
SkyMXF <skyf@192.168.2.104>
2020-06-09 17:21:36 +0800

    c

--- hello.py
+++
@@ -1,7 +0,0 @@
-
-
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    print(environ["PATH_INFO"])
-    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
-    return [body.encode('utf-8')]
