6f924b81c61261fed52e352f9d86afa373e652d2
SkyF <SkyMXF@users.noreply.github.com>
2020-06-09 19:45:47 +0800

    c

--- user/models.py
+++ user/models.py
@@ -5,3 +5,8 @@ class UserInfo(models.Model):
     user_qq_id = models.BigIntegerField()
     qq_group_id = models.BigIntegerField()
     nickname = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "qq: %s, group id: %s, nickname: %s"%(
+            self.user_qq_id, self.qq_group_id, self.nickname
+        )
