88bf8390f98dcdde52d9a857ecfb6fdff0d7ba20
SkyMXF <skyf@192.168.2.105>
2020-06-11 20:28:13 +0800

    c

--- user/forms.py
+++ user/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class UserLoginForm(forms.Form):
-    userqq = forms.CharField(label="QQ：", max_length=20, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "QQ",'autofocus': ''}))
-    password = forms.CharField(label="密码(不是QQ密码)：", max_length=20, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': "密码(不是QQ密码)"}))
+    userqq = forms.CharField(label="QQ", max_length=20, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "QQ"}))
+    password = forms.CharField(label="密码(不是QQ密码)", max_length=20, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': "密码(不是QQ密码)"}))
 
 class SetPasswordForm(forms.Form):
     password = forms.CharField(label="新的登录密码", max_length=20, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': "新的登录密码"}))
