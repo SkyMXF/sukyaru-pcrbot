e119cd0f33fcf20cc470d7314343e0963b375d49
SkyMXF <skyf@192.168.2.105>
2020-06-11 20:03:22 +0800

    c

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.userinfo, name='userinfo'),
     path('login', views.login, name='login'),
-    path('setpwd', views.setpwd, name='set_password')
+    path('setpwd', views.setpwd, name='set_password'),
+    path('logout', views.logout, name='logout')
 ]
--- user/views.py
+++ user/views.py
@@ -6,9 +6,16 @@ from . import models
 
 # Create your views here.
 def userinfo(request):
+    if not request.session.get('is_login', None):
+        # 未登录
+        return redirect('user/login/')
+
     return render(request, 'user/userinfo.html')
 
 def login(request):
+
+    if request.session.get('is_login', None):  # 不允许重复登录
+        return redirect('user/')
 
     if request.method == "POST":
         login_form = forms.UserLoginForm(request.POST)
@@ -17,7 +24,6 @@ def login(request):
             password = login_form.cleaned_data.get('password')
             # 验证输入qq为数字
             try:
-                print(userqq)
                 userqq = int(userqq)
             except:
                 message = "输入QQ号格式错误"
@@ -31,7 +37,18 @@ def login(request):
                 return render(request, 'user/login.html', {"message": message, "login_form": login_form})
             
             if user.password == password:
-                return redirect("/")
+                request.session['is_login'] = True
+                request.session['userqq'] = user.user_qq_id
+                request.session['user_name'] = user.nickname
+                user_auth_str = "Unknown"
+                if user.user_auth == 0:
+                    user_auth_str = "会长"
+                elif user.user_auth == 1:
+                    user_auth_str = "管理员"
+                elif user.user_auth == 2:
+                    user_auth_str = "普通群员"
+                request.session['user_auth'] = user_auth_str
+                return redirect("user")
             else:
                 message = "密码不正确，如果忘记可私聊发送'重置密码'进行重置"
                 return render(request, 'user/login.html', {"message": message, "login_form": login_form})
@@ -45,7 +62,42 @@ def login(request):
     return render(request, 'user/login.html', {"login_form": login_form})
 
 def setpwd(request):
-    return render(request, 'user/setpwd.html')
+    if not request.session.get('is_login', None):
+        # 未登录
+        return redirect("user/login/")
+    
+    if request.method == "POST":
+        setpwd_form = forms.SetPasswordForm(request.POST)
+        if setpwd_form.is_valid():
+            password = setpwd_form.cleaned_data.get('password')
+            retype_password = setpwd_form.cleaned_data.get('retype_password')
+
+            if password != retype_password:
+                message = "两次输入的密码不一致"
+                return render(request, 'user/setpwd.html', {"message": message})
+            
+            # 修改数据库    
+            userqq = request.session['userqq']
+            try:
+                userinfo = models.UserInfo.objects.get(user_qq_id=userqq)
+            except:
+                message = "Unknown ERROR: No record for this qq."
+                return render(request, 'user/setpwd.html', {"message": message})
+            userinfo.password = password
+            userinfo.save()
+                
+            return redirect("user/logout/")
+
+        else:
+            message = "密码输入格式错误"
+            return render(request, "user/setpwd.html", {"message": message})
+
+    setpwd_form = forms.SetPasswordForm()
+    return render(request, 'user/setpwd.html', {"setpwd_form": setpwd_form})
 
 def logout(request):
-    return redirect("/login/")
+    if not request.session.get('is_login', None):
+        # 未登录
+        return redirect("user/login/")
+    request.session.flush()
+    return redirect("user/login/")
