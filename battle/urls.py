1e7201e0c38839b042462beabe0def3ef472e665
SkyF <SkyMXF@users.noreply.github.com>
2020-06-14 01:12:18 +0800

    c

--- battle/urls.py
+++ battle/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('', views.index, name='index'),
+    path('', views.mybattle, name='mybattle'),
+    path('mybattle', views.mybattle, name='mybattle'),
+    path('guildbattle', views.guildbattle, name='guildbattle')
 ]
--- battle/views.py
+++ battle/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the battle index.")
+def mybattle(request):
+    if not request.session.get('is_login', None):
+        # 未登录
+        return redirect('/user/login')
+
+    return render(request, 'battle/mybattle.html')
+
+def guildbattle(request):
+    if not request.session.get('is_login', None):
+        # 未登录
+        return redirect('/user/login')
+
+    return render(request, 'battle/guildbattle.html')
