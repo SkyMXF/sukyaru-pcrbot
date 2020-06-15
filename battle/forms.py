1545fec767a907ee6977a920814cdce026ffc834
SkyF <SkyMXF@users.noreply.github.com>
2020-06-15 18:02:07 +0800

    c

--- battle/forms.py
+++ battle/forms.py
@@ -5,5 +5,5 @@ class BattleRecordForm(forms.Form):
     boss_id = forms.IntegerField(label="BOSS编号(1-5)", widget=forms.TextInput(attrs={'placeholder': "BOSS编号(1-5)"}))
     damage = forms.IntegerField(label="伤害", widget=forms.TextInput(attrs={'placeholder': "伤害"}))
     record_date = forms.DateTimeField(label="时间", widget=forms.DateTimeInput(attrs={'type':'datetime-local', 'placeholder': "出刀时间"}))
-    final_kill = forms.BooleanField(label="是尾刀", widget=forms.CheckboxInput(attrs={'type':'checkbox'}))
-    comp_flag = forms.BooleanField(label="是补偿刀", widget=forms.CheckboxInput(attrs={'type':'checkbox'}))
+    final_kill = forms.BooleanField(label="是尾刀", widget=forms.CheckboxInput(attrs={'type':'checkbox', 'value': "尾刀"}))
+    comp_flag = forms.BooleanField(label="是补偿刀", widget=forms.CheckboxInput(attrs={'type':'checkbox', 'value': "补偿刀"}))
