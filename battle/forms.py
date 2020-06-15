5d8a5a60fb5829b372e1bca8f1e333d7b5e01069
SkyF <SkyMXF@users.noreply.github.com>
2020-06-15 23:31:47 +0800

    c

--- battle/forms.py
+++ battle/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class BattleRecordForm(forms.Form):
     boss_stage = forms.IntegerField(label="周目数", widget=forms.TextInput(attrs={'placeholder': "周目数"}))
     boss_id = forms.IntegerField(label="BOSS编号(1-5)", widget=forms.TextInput(attrs={'placeholder': "BOSS编号(1-5)"}))
-    damage = forms.IntegerField(label="伤害", widget=forms.TextInput(attrs={'placeholder': "伤害"}))
-    record_date = forms.DateTimeField(label="时间", widget=forms.DateTimeInput(attrs={'type':'datetime-local', 'placeholder': "出刀时间"}))
+    damage = forms.IntegerField(label="伤害(填写数值,不能使用'w'/'W'等简称)", widget=forms.TextInput(attrs={'placeholder': "伤害"}))
+    record_date = forms.DateTimeField(label="时间", widget=forms.DateTimeInput(attrs={'type':'datetime-local'}))
     final_kill = forms.BooleanField(label="尾刀", label_suffix="")
     comp_flag = forms.BooleanField(label="补偿刀", label_suffix="")
