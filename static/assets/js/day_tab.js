e0b5073a28228be6e318120f0e429fed1effb63b
SkyF <SkyMXF@users.noreply.github.com>
2020-06-15 22:05:13 +0800

    c

--- static/assets/js/day_tab.js
+++ static/assets/js/day_tab.js
@@ -1,5 +1,7 @@
-var tabs = document.getElementsByClassName('tab-head')[0].getElementsByTagName('h3'),
-contents = document.getElementsByClassName('tab-content')[0].getElementsByTagName('div');
+var tabs = document.getElementsByClassName('tab-head')[0].getElementsByClassName('selected');
+tabs = tabs.concat(document.getElementsByClassName('tab-head')[0].getElementsByClassName('unselected'));
+contents = document.getElementsByClassName('tab-content')[0].getElementsByClassName('show');
+contents = contents.concat(document.getElementsByClassName('tab-content')[0].getElementsByClassName('hide'));
 
 (function changeTab(tab) {
     for(var i = 0, len = tabs.length; i < len; i++) {
@@ -13,8 +15,8 @@ function showTab() {
             tabs[i].className = 'selected';
             contents[i].className = 'show';
         } else {
-            tabs[i].className = '';
-            contents[i].className = '';
+            tabs[i].className = 'unselected';
+            contents[i].className = 'hide';
         }
     }
 }
