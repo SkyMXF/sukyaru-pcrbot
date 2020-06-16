1ff3876338f7534806f1b746770c35d164e9bdb0
SkyF <SkyMXF@users.noreply.github.com>
2020-06-17 01:44:33 +0800

    c

--- static/assets/js/table_sort.js
+++ static/assets/js/table_sort.js
@@ -1,5 +1,5 @@
 $(document).on('click','th',function(){
-    var table = $(this).parents('table').eq(0);
+    var table = $(this).parents('alt').eq(0);
     var rows = table.find('tr:gt(1)').toArray().sort(comparer($(this).index()));
     this.asc = !this.asc;
     if (!this.asc){rows = rows.reverse();}
