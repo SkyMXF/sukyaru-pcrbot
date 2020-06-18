4864f58cd550ebdade2c6eae23a39f73060524f5
SkyF <SkyMXF@users.noreply.github.com>
2020-06-18 22:27:19 +0800

    c

--- battle/admin.py
+++ battle/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import BossStatus, BossInfo, NowBattleRecord, BattleDate
+from .models import BossStatus, BossInfo, NowBattleRecord, BattleDate, NowBattleBoss
 
 # Register your models here.
 admin.site.register(BossStatus)
 admin.site.register(BossInfo)
 admin.site.register(NowBattleRecord)
 admin.site.register(BattleDate)
+admin.site.register(NowBattleBoss)
