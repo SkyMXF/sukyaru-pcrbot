aa2862fd49f9d6e5566455b6d66acb3702de8afe
SkyMXF <skyf@192.168.2.102>
2020-06-18 15:11:36 +0800

    c

--- battle/views.py
+++ battle/views.py
@@ -61,9 +61,9 @@ def mybattle(request):
                 )
 
         # 查询他人记录
-        query_qq = request.GET.get("queryqq", None)
+        get_query_qq = request.GET.get("queryqq", None)
         if query_qq:
-            query_qq = int(query_qq)
+            query_qq = int(get_query_qq)
             if query_qq != request.session["userqq"]:
                 show_dict["self_page"] = False
     
