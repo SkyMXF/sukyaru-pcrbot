4f85f85324a520a68eb0374b158c016d41f4e547
SkyF <SkyMXF@users.noreply.github.com>
2020-06-18 21:58:30 +0800

    c

--- guild/admin.py
+++ guild/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Title, UserTitle, Announcement
+from .models import TitleName, Title, UserTitle, Announcement
 
 # Register your models here.
+admin.site.register(TitleName)
 admin.site.register(Title)
 admin.site.register(UserTitle)
 admin.site.register(Announcement)
