be269ec9ff649721f75c8aafa98a48b4747d9737
SkyMXF <skyf@192.168.2.102>
2020-06-18 16:23:27 +0800

    c

--- battle/forms.py
+++ battle/forms.py
@@ -5,5 +5,5 @@ class BattleRecordForm(forms.Form):
     boss_id = forms.IntegerField(label="BOSS编号(1-5)", widget=forms.TextInput(attrs={'placeholder': "BOSS编号(1-5)"}))
     damage = forms.IntegerField(label="伤害(填写数值,不能使用'w'/'W'等简称)", widget=forms.TextInput(attrs={'placeholder': "伤害"}))
     record_date = forms.DateTimeField(label="时间", widget=forms.DateTimeInput(attrs={'type':'datetime-local'}, format='%Y-%m-%d %H:%M'), input_formats='%Y-%m-%d %H:%M')
-    final_kill = forms.BooleanField(label="尾刀", label_suffix="")
-    comp_flag = forms.BooleanField(label="补偿刀", label_suffix="")
+    final_kill = forms.BooleanField(label="尾刀", label_suffix="", required=False)
+    comp_flag = forms.BooleanField(label="补偿刀", label_suffix="", required=False)
