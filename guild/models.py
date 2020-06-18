6a803abc4843dd86e93506bcae3f873d6fa5cd0f
SkyF <SkyMXF@users.noreply.github.com>
2020-06-18 21:59:28 +0800

    c

--- guild/models.py
+++ guild/models.py
@@ -6,8 +6,8 @@ class TitleName(models.Model):
     name = models.CharField(max_length=30)    # 称号名称
 
     def __str__(self):
-        return "id_%d, %s"%(name)
-        
+        return "id_%d, %s"%(self.name)
+
 class Title(models.Model):
     title_prefix = models.CharField(max_length=30)  # 称号前缀（如：第X期，在部分页面不显示）
     title_name = models.ForeignKey(TitleName, on_delete=models.CASCADE)    # 称号名称
