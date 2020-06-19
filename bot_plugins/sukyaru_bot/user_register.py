18cdb5832d21b69ea5f84f9443b26516b82ca429
SkyF <SkyMXF@users.noreply.github.com>
2020-06-19 23:13:40 +0800

    c

--- bot_plugins/sukyaru_bot/user_register.py
+++ bot_plugins/sukyaru_bot/user_register.py
@@ -28,4 +28,34 @@ async def register_parser(session: CommandSession):
 
     session.state["user_qq"] = session.event.sender["user_id"]
     session.state["nickname"] = session.event.sender["nickname"]
-    
+
+@on_command(name="guild_register", aliases=("公会注册"))
+async def guild_register(session: CommandSession):
+    # args：
+    # qq
+    # nickname
+    # auth -暂不需要，默认为2(普通群员)
+
+    print(session.bot.get_group_list())
+
+    '''
+    success, message = user_register.register_user(
+        user_info_dict={
+            "user_qq": int(session.state["user_qq"]),
+            "password": configs.reg_default_pwd,
+            "nickname": session.state["nickname"],
+            "user_auth": 2
+        }
+    )
+    '''
+
+    await session.send(
+        "公会注册假装成功"
+    )
+
+@guild_register.args_parser
+async def guild_register_parser(session: CommandSession):
+    #arg_text = session.current_arg_text.strip()
+
+    session.state["user_qq"] = session.event.sender["user_id"]
+    session.state["nickname"] = session.event.sender["nickname"]
