821abc9c7cc4ad749988190dbcd60b2959e2353a
SkyF <SkyMXF@users.noreply.github.com>
2020-06-20 21:31:14 +0800

    c

--- main/utils.py
+++ main/utils.py
@@ -144,9 +144,11 @@ def upload_battle_record(record_dict):
                 record_date__gte=record_date.day_begin(),
                 record_date__lt=record_date.day_end(),
                 comp_flag=False
-            )
+            ).order_by(record_date)
             if len(user_today_battle_record_set) >= 3 and record_dict["comp_flag"] == False:
                 raise ValueError("报刀失败：骑士君这一天已经有3次非补偿刀的报刀了噢")
+            if user_today_battle_record_set.last().comp_flag and record_dict["comp_flag"]:
+                raise ValueError("报刀失败：最近的一次报告也是补偿刀，不可以连续报告补偿刀噢")
 
             # 生成record
             upload_record = NowBattleRecord.objects.create(
