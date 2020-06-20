e30000eb7de5bd1d743d470bbc302ad7f51f7d2d
SkyF <SkyMXF@users.noreply.github.com>
2020-06-20 16:09:33 +0800

    c

--- bot_plugins/sukyaru_bot/clan_battle.py
+++ bot_plugins/sukyaru_bot/clan_battle.py
@@ -7,14 +7,24 @@ import re
 from .lib import configs, battle_report
 import bot_config
 
+report_example = "报刀 1-1 876543 或 报刀 1-1 54321 补偿"
+
 @on_command(name="report", aliases=("报刀"))
 async def report(session: CommandSession):
-    # args：
-    # qq
-    # nickname
-    # auth -暂不需要，默认为2(普通群员)
-    await session.send(session.state["test_field"])
-    return
+    
+    if not session.state["valid_report"]:
+        await session.send("报刀格式有误噢~示例：%s"%(report_example))
+        return
+
+    # 输入格式检查
+    try:
+        session.state["user_qq"] = int(session.state["user_qq"])
+        session.state["boss_real_stage"] = int(session.state["boss_real_stage"])
+        session.state["boss_id"] = int(session.state["boss_id"])
+        session.state["damage"] = int(session.state["damage"])
+        session.state["operator_qq"] = int(session.state["operator_qq"])
+    except:
+        await session.send("报刀格式有误噢~示例：%s"%(report_example))
 
     success, message = battle_report.report(
         battle_record_dict={
@@ -24,7 +34,8 @@ async def report(session: CommandSession):
             "damage": session.state["damage"],
             "record_date": session.state["record_date"],
             "final_kill": session.state["final_kill"],
-            "comp_flag": session.state["comp_flag"]
+            "comp_flag": session.state["comp_flag"],
+            "operator_qq": session.state["operator_qq"]
         }
     )
 
@@ -32,26 +43,27 @@ async def report(session: CommandSession):
         "%s"%(message)
     )
 
-report_example = "报刀 1-1 876543 或 报刀 1-1 54321 补偿"
-
 @report.args_parser
 async def report_parser(session: CommandSession):
     arg_text = session.current_arg_text.strip()
 
-    arg_re = re.compile(r"\s*(?P<boss_real_stage>\d+)\-(?P<boss_id>\d+)\s+(?P<damage>\d+)\s*(?P<comp_flag>补偿)?\s*")
+    arg_re = re.compile(r"\s*(?P<boss_real_stage>\d+)\-(?P<boss_id>\d+)\s+(?P<damage>\d+)\s+(?P<comp_flag>补偿)?\s+qq=(?P<record_qq>)\s*")
     match_args = re.search(arg_re, arg_text)
 
     if match_args:
-        session.state["test_field"] = "stage %s, boss_id %s, damage %s, comp_flag %s"%(
-            match_args.group("boss_real_stage"),
-            match_args.group("boss_id"),
-            match_args.group("damage"),
-            match_args.group("comp_flag"),
-        )
-    else:
-        session.state["test_field"] = "不匹配噢"
-    return
+        # 格式正确
+        session.state["valid_report"] = True    # 有效报刀
 
-    now_datetime = datetime.datetime.utcnow()
-    session.state["user_qq"] = session.event.sender["user_id"]
-    session.state["record_date"] = now_datetime
+        now_datetime = datetime.datetime.utcnow()
+
+        session.state["user_qq"] = session.event.sender["user_id"] if match_args.group("record_qq") is None else match_args.group("record_qq")
+        session.state["operator_qq"] = session.event.sender["user_id"]
+        session.state["boss_real_stage"] = match_args.group("boss_real_stage")
+        session.state["boss_id"] = match_args.group("boss_id")
+        session.state["damage"] = match_args.group("damage")
+        session.state["record_date"] = now_datetime
+        session.state["final_kill"] = False     # 暂不使用
+        session.state["comp_flag"] = False if match_args.group("comp_flag") != "补偿" else True
+    else:
+        session.state["valid_report"] = False
+
--- bot_plugins/sukyaru_bot/lib/battle_report.py
+++ bot_plugins/sukyaru_bot/lib/battle_report.py
@@ -1,5 +1,41 @@
-
+from user.models import UserInfo
+from battle.models import BossStatus
 from main import utils
 
 def report(battle_record_dict):
-    return True, "成功"
+
+    # 权限检查
+    try:
+        operator_info = UserInfo.objects.get(user_qq_id=battle_record_dict["operator_qq"])
+    except:
+        return False, "报刀失败：凯露酱这里没有操作者的qq的记录噢"
+    if battle_record_dict["operator_qq"] != battle_record_dict["user_qq"] and operator_info.user_auth >= 2:    # 操作者非本人或管理员
+        return False, "报刀失败：骑士君没有代替报刀的权限噢~"
+
+    # 调用utils函数
+    try:
+        utils.upload_battle_record(battle_record_dict)
+    except ValueError as e:
+        return False, str(e)
+
+    # TODO: 烂代码：获取出刀玩家名称
+    try:
+        user_info = UserInfo.objects.get(user_qq_id=battle_record_dict["user_qq"])
+    except:
+        return False, "报刀失败：出现异常错误，请再试一次或联系管理员"
+
+    # TODO: 烂代码：获取boss信息
+    try:
+        boss_status = BossStatus.objects.get(boss_real_stage=battle_record_dict["boss_real_stage"], boss_info__boss_id=battle_record_dict["boss_id"])
+    except:
+        return False, "报刀失败：出现异常错误，请再试一次或联系管理员"
+    
+    return True, "报刀成功：%s对BOSS%s(%d-%d)造成了%d点伤害，BOSS当前血量：%d/%d"%(
+        user_info.nickname,
+        boss_status.boss_info.boss_name,
+        battle_record_dict["boss_real_stage"],
+        battle_record_dict["boss_id"],
+        battle_record_dict["damage"],
+        boss_status.health,
+        boss_status.boss_info.total_health
+    )
