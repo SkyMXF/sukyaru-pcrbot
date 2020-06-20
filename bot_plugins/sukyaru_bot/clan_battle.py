17a680075c695e359d716f9f37681cfa17f3d910
SkyF <SkyMXF@users.noreply.github.com>
2020-06-20 21:09:25 +0800

    c

--- bot_plugins/sukyaru_bot/clan_battle.py
+++ bot_plugins/sukyaru_bot/clan_battle.py
@@ -131,6 +131,6 @@ async def battle_sl(session: CommandSession):
 async def battle_sl_parser(session: CommandSession):
     session.state["user_qq"] = session.event.sender["user_id"]
 
-@nonebot.scheduler.scheduled_job("cron", hour=5, min=9)
+@nonebot.scheduler.scheduled_job("cron", hour=21, min=11)
 async def reset_every_member_sl():
     sl_record.reset_all_sl()
