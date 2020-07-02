9d3b31d4b27a6ed97d70d06d492da53a2e15e8e0
SkyF <SkyMXF@users.noreply.github.com>
2020-07-02 13:53:08 +0800

    c

--- static/assets/js/table_sort.js
+++ static/assets/js/table_sort.js
@@ -1,8 +1,36 @@
 $(document).on('click','th',function(){
     var table = $(this).parents('table').eq(0);
-    console.log("index表",$(this).index())
-    console.log("this信息", $(this).text())
-    var rows = table.find('tr:gt(1)').toArray().sort(comparer($(this).index()));
+
+    
+    var index_in_html = $(this).index()
+    var col_name_in_html = $(this).text()
+    var real_index = index_in_html
+
+    var head_row_num = table.find("thead").find("tr").toArray().length
+    if (head_row_num > 1){
+        // 是复合表头，需要计算实际排序依据列号
+        switch(index_in_html){
+            case 0:
+                if (col_name_in_html === "昵称") real_index = 0;    // 昵称
+                else if (col_name_in_html === "伤害") real_index = 1; // 第一刀伤害
+            case 1:
+                if (col_name_in_html === "第一刀") real_index = 1;    // 第一刀伤害
+                else if (col_name_in_html === "补偿刀") real_index = 2; // 第一刀补偿刀
+            case 2:
+                real_index = 3; // 第二刀伤害
+            case 3:
+                if (col_name_in_html === "第三刀") real_index = 5;    // 第三刀伤害
+                else if (col_name_in_html === "补偿刀") real_index = 4; // 第二刀补偿刀
+            case 4:
+                if (col_name_in_html === "积分") real_index = 7;    // 积分
+                else if (col_name_in_html === "伤害") real_index = 5; // 第三刀伤害
+            case 5:
+                real_index = 6; // 第三刀补偿刀
+        }
+    }
+
+    // 排序
+    var rows = table.find("tbody").find('tr').toArray().sort(comparer(real_index));
     this.asc = !this.asc;
     if (!this.asc){rows = rows.reverse();}
     table.children('tbody').empty().html(rows);
@@ -15,6 +43,7 @@ function comparer(index) {
     };
 }
 var name_damage_re = /\(\d\-\d\)(\d+)/;
+var only_number_re = /^(\d+)(\.\d+)?$/;
 function getCellValue(row, index){
     console.log("行列", row, index)
     var text_value = $(row).children('td').eq(index).text()
@@ -29,17 +58,15 @@ function getCellValue(row, index){
             console.log("匹配成功", value)
         }
         else{
-            // 匹配失败，直接转int
-            value = parseFloat(text_value)
-            console.log("匹配失败", value)
-            if (isNaN(value)){
-                console.log("发现NaN")
-                value = text_value;
+            // 匹配boss名+伤害的re失败, 尝试匹配纯数字
+            if (text_value.match(only_number_re)){
+                value = parseFloat(text_value)
+            }
+            else{
+                // 匹配纯数字失败，认为是无需转换的字符串
+                value = text_value
             }
         }
-    }
-    else{
-        console.log("空字符", value)
     }
     console.log(value)
     return value;
